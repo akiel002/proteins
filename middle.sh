c823148d114f2016a72f1be8d8967fa36f209092
Alethia Kielbasa <alethia.kielbasa@ou.edu>
2022-06-13 15:58:04 +0000

    added ability to insert file and numbers in positions 1 2 and 3

--- middle.sh
+++ middle.sh
@@ -1,4 +1,4 @@
 # Script to print out the middle of a file
 
-head -n 15 octane.pdb | tail -n 5
+head -n "$2" "$1" | tail -n "$3" # $1 is position one and let's you input any file name, $2 is a number that pases the head and $3 is tail
 
